552d19a736656b68ac8ad44bd3dcfc265cfcc601
Mohanakrishna <mohanakrishnavh@gmail.com>
2020-02-01 21:03:57 -0500

    Two Sum

---
+++ Leetcode/two_sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    n = len(nums)
+    complement_map = {nums[0]: 0}
+
+    for i in range(1, n):
+        complement = target - nums[i]
+
+        if complement in complement_map:
+            return [complement_map[complement], i]
+        else:
+            complement_map[nums[i]] = i
+
+
+print(two_sum([2, 7, 11, 15], 9))
